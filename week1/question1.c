4bfe85e7ab5bfb331089110ee541a255341753df
Sehaj <sehajimal@gmail.com>
2024-09-17 14:07:26 -0400

    First Commit

---
+++ week1/question1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() 
+{
+    printf("Sehaj ");
+    fflush(stdout);
+    sleep(1);
+
+    printf("Ajimal ");
+    fflush(stdout);
+    sleep(1);
+
+    printf("400511220 ");
+    fflush(stdout);
+    sleep(1);
+
+    printf("SFWRENG ");
+    fflush(stdout);
+    sleep(1);
+
+    printf("2XC3: ");
+    fflush(stdout);
+    sleep(1);
+
+    printf("Development ");
+    fflush(stdout);
+    sleep(1);
+
+    puts("Basics ");
+    fflush(stdout);
+    sleep(1);
+
+    puts(" ");
+    puts("What do you think is the most useful thing you’ve learned so far in this course? ");
+    fflush(stdout);
+    sleep(7);
+
+    puts(" ");
+    puts("The most useful thing I have learned so far is the idea that I can make directories and put scripts inside to accomplish a task that I desire. For example the script I made that changes to a directory specified by me and deletes a certian type of file is pretty cool. It just shows me that there is so much I can accomplish. ");
+    fflush(stdout);
+    sleep(17);
+
+    puts(" ");
+    puts("Is there anything you’ve learned so far that you do not think is useful? If so, explain why. ");
+    fflush(stdout);
+    sleep(7);
+
+    puts(" ");
+    puts("So far I think everything I have learned so far has been useful. Even if there may have been such commands that I did not use, they are still good to know so that I can understand what the capabilities are of the linux command line. ");
+    fflush(stdout);
+    sleep(17);
+
+    puts(" ");
+    puts("What are the advantages and disadvantages of using vim for coding? ");
+    fflush(stdout);
+    sleep(7);
+
+    puts(" ");
+    puts("I think that the advantage of using vim include the fact that I feel fully connected to the code as I am getting no help by autowriting some text like VS code would do. This will enable me to fully learn C without any help which will make me a better programmer in general. The disadvantage is the fact that it takes time to save and exit vim. I have to write some commands to even insert and then save and quit which can be seen as a waste of time.");
+    fflush(stdout);
+    sleep(20);
+
+    puts(" ");
+    puts("What are the advantages and disadvantages of using VS Code for coding?");
+    fflush(stdout);
+    sleep(7);
+
+    puts(" ");
+    puts("I think that the advantage of using VS code for coding is the fact that it gives me expereince with using an IDE. It has a lot of conviniences and tools which enable me to simply code and not have to worry about a lot of the things that are going on in the background. The disadvantage would be the fact that I might rely too much on these tools which will not allow me to learn about the true side of programming.");
+    fflush(stdout);
+    sleep(20);
+
+
+
+    return EXIT_SUCCESS;
+}
